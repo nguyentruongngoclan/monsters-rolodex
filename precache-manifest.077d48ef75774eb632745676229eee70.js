15a091a47ae59028cee37674562d73f6c9f2ad48
lannguyen <nguyentruongngoclan@gmail.com>
2019-12-28 21:07:28 -0800

    Updates

---
+++ precache-manifest.077d48ef75774eb632745676229eee70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33986ea50034be358096e9141d328da6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8093e4e6ccc85c78c70",
+    "url": "/monsters-rolodex/static/css/main.9ef181a2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8093e4e6ccc85c78c70",
+    "url": "/monsters-rolodex/static/js/main.1210185b.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
